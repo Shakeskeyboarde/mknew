77219e340c83552d3272d345dec79cbfc52a3233
Chris Ackerman <chris@topher.land>
2022-02-09 10:04:03 -0800

    Improved error handling

--- src/bin.ts
+++ src/bin.ts
@@ -1,6 +1,20 @@
 #!/usr/bin/env node
-/* istanbul ignore file */
-import 'source-map-support/register';
-import { main } from './main';
 
-main();
+/* istanbul ignore file */
+
+Promise.resolve()
+  .then(async () => {
+    if (process.env.NODE_ENV === 'development') {
+      try {
+        // eslint-disable-next-line import/no-extraneous-dependencies
+        await import('source-map-support/register');
+      } catch {
+        // The source-map-support dependency is optional, and only installed
+        // in development.
+      }
+    }
+
+    return undefined;
+  })
+  .then(async () => import('./main'))
+  .then(async ({ main }) => main());
